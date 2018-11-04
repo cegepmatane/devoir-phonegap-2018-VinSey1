529eb95360ac8bfd6cf1fdb87a8b2f036b14d0c3
Vincent Seyller <vseyller@laposte.net>
2018-11-03 20:57:36 -0400

    Création vue des détails d'un personnage

---
+++ app/src/main/assets/www/js/vue/PersonnageVue.js
@@ -0,0 +1,18 @@
+var PersonnageVue = (function() {
+    var pagePersonnage = document.getElementById("page-personnage").innerHTML;
+    classe = function (personnage) {
+        var personnageNom;
+        var personnageRaceEtClasse;
+        var personnageNiveau;
+        this.afficher = function () {
+            document.getElementsByTagName("body")[0].innerHTML = pagePersonnage;
+            personnageNom = document.getElementById("personnage-nom");
+            personnageNom.innerHTML = personnage.nom;
+            personnageRaceEtClasse = document.getElementById("personnage-race-classe");
+            personnageRaceEtClasse.innerHTML = personnage.race + " " + personnage.classe;
+            personnageNiveau = document.getElementById("personnage-niveau");
+            personnageNiveau.innerHTML = "Niveau : " + personnage.niveau;
+        }
+    };
+    return classe;
+})();
