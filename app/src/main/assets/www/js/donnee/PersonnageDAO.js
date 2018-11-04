6a4498260c3d20b9fc1a48b092715c218a90bcdc
Vincent Seyller <vseyller@laposte.net>
2018-11-03 20:56:56 -0400

    Ajout méthode chercherAvecId dans le DAO

--- app/src/main/assets/www/js/donnee/PersonnageDAO.js
+++ app/src/main/assets/www/js/donnee/PersonnageDAO.js
@@ -1,10 +1,22 @@
 var PersonnageDAO = function () {
-    this.listePersonnage = [
-        new Personnage(0, "Eliott", "Quart d'elfe", "Archimage", 120),
-        new Personnage(1, "Vincent", "Humain", "Prêtre", 120),
-        new Personnage(2, "Youssef", "Humain", "Voleur", 1),
-    ]
-    this.lister = function () {
-        return this.listePersonnage;
+    var listePersonnage;
+    var initialiser = function () {
+        if (!listePersonnage) {
+            listePersonnage = [
+                new Personnage(0, "Eliott", "Quart d'elfe", "Archimage", 120),
+                new Personnage(1, "Vincent", "Humain", "Prêtre", 120),
+                new Personnage(2, "Youssef", "Humain", "Voleur", 1)
+            ]
+        }
     }
+    this.chercherAvecId = function (idPersonnage) {
+        for (position in listePersonnage) {
+            if (listePersonnage[position].id == idPersonnage)  return listePersonnage[position];
+        }
+        return listePersonnage[0];
+    }
+    this.lister = function () {
+        return listePersonnage;
+    }
+    initialiser();
 }
